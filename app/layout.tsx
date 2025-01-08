ed41681f99c56deff25d71517e81018622e7fb76
Sakai-To241202 <sprsnqlbrm@gmail.com>
2025-01-08 10:40:31 +0900

    gtm連携を、@next/third-partiesに変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Header from './_components/Header';
-import Footer from './_components/Footer';
+import { GoogleTagManager } from "@next/third-parties/google";
+import Header from "./_components/Header";
+import Footer from "./_components/Footer";
 
 export const metadata: Metadata = {
   title: "Portfolio",
@@ -14,37 +15,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja"> {/* 日本語の場合は lang="ja" に変更 */}
-      <head>
-        {/* GTM HEAD スクリプト */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){
-                w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});
-                var f=d.getElementsByTagName(s)[0],
-                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-                j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-5N5JNJM9');`, // GTMのIDを設定
-          }}
-        />
-      </head>
+    <html lang="ja">
+      {" "}
+      {/* 日本語の場合は lang="ja" に変更 */}
       <body>
-        {/* GTM BODY スクリプト */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5N5JNJM9"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
-        </noscript>
         <Header />
         {children}
         <Footer />
       </body>
+      <GoogleTagManager gtmId="GTM-5N5JNJM9" />
     </html>
   );
 }
