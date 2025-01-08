545d3c9ef4ef108bbf6bb9845ee77d93a77e6e8b
Sakai-To241202 <sprsnqlbrm@gmail.com>
2025-01-08 09:50:29 +0900

    gtm追加

--- app/page.tsx
+++ app/page.tsx
@@ -6,53 +6,53 @@ import NewsList from "@/app/_components/NewsList";
 import { News } from '@/app/_libs/microcms';
 
 
-const data: {
-  contents: News[]} = {
-    contents: [
-      {
-        id: "1",
-        title: 
-        "渋谷にオフィスを移転しました。",
-        category: {
-          name: "更新情報",
-        },
-        publishedAt: "2025/01/01",
-        createdAt: "2025/01/01",
-      },
-    {
-      id:"2",
-      title: "当社CEOが業界リーダーTOP30に選出されました",
-      category: {
-        name: "更新情報",
-      },
-      publishedAt: "2025/01/01",
-      createdAt: "2025/01/01",
-    },
-      {
-        id:"3",
-        title: "テストの記事です",
-        category: {
-          name: "更新情報",
-        },
-      publishedAt: "2025/01/01",
-      createdAt: "2025/01/01",
-    },
-      {
-        id:"4",
-        title: "テストの記事です",
-        category: {
-          name: "更新情報",
-        },
-      publishedAt: "2025/01/01",
-      createdAt: "2025/01/01",
-    },
-    ],
-  };
+// const data: {
+//   contents: News[]} = {
+//     contents: [
+//       {
+//         id: "1",
+//         title: 
+//         "渋谷にオフィスを移転しました。",
+//         category: {
+//           name: "更新情報",
+//         },
+//         publishedAt: "2025/01/01",
+//         createdAt: "2025/01/01",
+//       },
+//     {
+//       id:"2",
+//       title: "当社CEOが業界リーダーTOP30に選出されました",
+//       category: {
+//         name: "更新情報",
+//       },
+//       publishedAt: "2025/01/01",
+//       createdAt: "2025/01/01",
+//     },
+//       {
+//         id:"3",
+//         title: "テストの記事です",
+//         category: {
+//           name: "更新情報",
+//         },
+//       publishedAt: "2025/01/01",
+//       createdAt: "2025/01/01",
+//     },
+//       {
+//         id:"4",
+//         title: "テストの記事です",
+//         category: {
+//           name: "更新情報",
+//         },
+//       publishedAt: "2025/01/01",
+//       createdAt: "2025/01/01",
+//     },
+//     ],
+//   };
 
 
 export default function Home() {
   const name = "市場をリードしている、グローバルテックカンパニー";
-  const sliceData = data.contents.slice(0,4);
+  // const sliceData = data.contents.slice(0,4);
   // const sliceData = [];
   return (
     <>
@@ -70,11 +70,11 @@ export default function Home() {
       />
     </section>
     <section className={styles.news}>
-        <h2 className={styles.newsTitle}>News</h2>
+        {/* <h2 className={styles.newsTitle}>News</h2>
         <NewsList news={sliceData}/>
         <div className={styles.newsLink}>
           <ButtonLink href="/news">もっとみる</ButtonLink>
-        </div>
+        </div> */}
       </section>
     </>
   );
